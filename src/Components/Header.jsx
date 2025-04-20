28fc9db38edcf745baa7417e98b39c27f0ca1277
Qunainb <waniqunain@gmail.com>
2025-04-20 09:39:21 +0530

    Cart logic with useContext and useReducer

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,23 @@
+import { useContext } from "react";
 import logoImg from "../assets/logo.jpg";
 import Button from "./UI/Button";
+import CartContext from "../store/CartContext";
 
 export default function Header() {
+  const { items } = useContext(CartContext);
+
+  const totalCartItems = items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
   return (
     <header id="main-header">
       <div id="title">
         <img src={logoImg} alt="Header-Image" />
-        <h1>Food App</h1>
+        <h1>Spice Bite</h1>
       </div>
       <nav>
-        <Button textOnly>Cart (0)</Button>
+        <Button textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
